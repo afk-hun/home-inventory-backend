e5911222a3121236ce659c9dda0912203da64684
AFK (Akos Ferenc Kalamar) <akos.kalamar@gmail.com>
2026-02-21 16:31:46 +0100

    feat: add signin endpoint and user to db
    I added a minimalist data and user validaton

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes  from "./routes/auth";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -9,8 +11,36 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const MONGODB_URI = `${process.env.MONGODB_TYPE}://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_SERVER_DOMAIN}:${process.env.MONGODB_PORT}/home-inventory?authSource=admin`;
 
+app.use(bodyParser.json()); 
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"OPTIONS, GET, POST, PUT, PATCH, DELETE",
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Content-Type, Authorization",
+	);
+	next();
+});
+
+app.use("/auth", authRoutes);
+
 app.get("/health", (_req, res) => {
 	res.json({ status: "ok" });
+});
+
+app.use((error: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+	const status = error.statusCode || 500;
+	const message = error.message || "Internal server error";
+	const data = error.data;
+
+	res.status(status).json({
+		message,
+		data,
+	});
 });
 
 if (!MONGODB_URI.includes("undefined")) {
---
+++ src/routes/auth.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import User from "../models/user";
+import { signup } from "../controller/auth";
+
+const router = Router();
+
+router.put(
+	"/signup",
+	[
+		body("email")
+			.isEmail()
+			.withMessage("Please enter a valid email.")
+			.custom((value, { req }) => {
+				return User.findOne({ email: value }).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject("E-Mail address already exists!");
+					}
+				});
+			})
+			.normalizeEmail(),
+		body("password").trim().isLength({ min: 6 }),
+		body("name").trim().not().isEmpty(),
+	],
+	signup,
+);
+
+export default router;
